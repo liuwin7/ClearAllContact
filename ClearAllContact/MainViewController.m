e036977d613d471ff39bfad4ef30338856c68b91
刘富东 <liuwin7@163.com>
2015-08-19 20:24:39 +0800

    add backup function

--- ClearAllContact/MainViewController.m
+++ ClearAllContact/MainViewController.m
@@ -8,9 +8,11 @@
 
 #import "MainViewController.h"
 #import <AddressBook/AddressBook.h>
+#import <AFNetworking/AFNetworking.h>
 
 NSString *CONTACT_NAME = @"ContactName";
 NSString *CONTACT_PHONE = @"ContactPhone";
+NSString *CONTACT_PHONE_LABEL = @"ConctactPhoneLabel";
 
 @interface MainViewController ()<UIAlertViewDelegate>
 
@@ -53,6 +55,15 @@ NSString *CONTACT_PHONE = @"ContactPhone";
 - (IBAction)importAction:(UIButton *)sender {
     [self importDefaultContact];
 }
+
+- (IBAction)backupAction:(UIButton *)sender {
+    [self backupContact];
+}
+
+- (IBAction)recoverAction:(UIButton *)sender {
+    [self recoverContact];
+}
+
 
 #pragma mark - UIAlertViewDelegate
 - (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
@@ -161,6 +172,117 @@ NSString *CONTACT_PHONE = @"ContactPhone";
             NSLog(@"error %@", (__bridge NSError *)error);
         }
     }
+}
+
+/**
+ *  备份通讯录中的内容，主要是姓名,电话,电话的标签(Label)
+ *  生成一个plist文件
+    name:张三
+    phone:{
+            [
+                {
+                     1352572727,
+                     住宅
+                },
+                {
+                     1352572727,
+                     办公
+                }
+ 
+            ]
+        }
+ */
+
+- (void)backupContact {
+    CFArrayRef recordArray = ABAddressBookCopyArrayOfAllPeople(addressBook);
+    long recordCount = CFArrayGetCount(recordArray);
+    NSMutableArray *contactArray = [NSMutableArray arrayWithCapacity:recordCount];
+    for (long i = 0; i < recordCount; i++) {
+        ABRecordRef record = CFArrayGetValueAtIndex(recordArray, i);
+        // 电话号码
+        ABMultiValueRef phoneRef = ABRecordCopyValue(record, kABPersonPhoneProperty);
+        long phoneCount = ABMultiValueGetCount(phoneRef);
+        NSMutableArray *allPhone = [NSMutableArray arrayWithCapacity:phoneCount]; // 存放所有的电话号码
+        for (long j = 0; j < phoneCount; j++) {
+            NSMutableDictionary *phoneDic = [NSMutableDictionary dictionaryWithCapacity:3];
+            CFStringRef phoneLabelRef = ABMultiValueCopyLabelAtIndex(phoneRef, j);
+            CFStringRef lLocalizedLabel = ABAddressBookCopyLocalizedLabel(phoneLabelRef);
+            CFStringRef phoneNumberRef = ABMultiValueCopyValueAtIndex(phoneRef, j);
+            if (lLocalizedLabel) {
+                phoneDic[CONTACT_PHONE_LABEL] = (__bridge NSString *)lLocalizedLabel;
+                CFRelease(lLocalizedLabel);
+            } else {
+                phoneDic[CONTACT_PHONE_LABEL] = @"";
+            }
+            if (phoneLabelRef) {
+                CFRelease(phoneLabelRef);
+            }
+            if (phoneNumberRef) {
+                phoneDic[CONTACT_PHONE] = (__bridge NSString *)phoneNumberRef;
+                CFRelease(phoneNumberRef);
+            } else {
+                phoneDic[CONTACT_PHONE] = @"";
+            }
+            
+            [allPhone addObject:phoneDic];
+        }
+        if (phoneRef) {
+            CFRelease(phoneRef);
+        }
+        
+        // 姓名
+        CFStringRef firstNameRef = ABRecordCopyValue(record, kABPersonFirstNameProperty);
+        CFStringRef lastNameRef = ABRecordCopyValue(record, kABPersonLastNameProperty);
+        NSString *firstName = firstNameRef != NULL ? (__bridge NSString *)firstNameRef : @"";
+        NSString *lastName = lastNameRef != NULL ? (__bridge NSString *)lastNameRef : @"";
+        NSString *contactName = [NSString stringWithFormat:@"%@%@", lastName, firstName]; // 姓名
+        
+        NSDictionary *dic = @{
+                              @"name": contactName,
+                              @"phone": allPhone,
+                              };
+        
+        [contactArray addObject:dic];
+    }
+    
+    NSString *backupFilePath = [NSHomeDirectory() stringByAppendingString:@"/Documents/conatct_backup.plist"];
+    if (contactArray.count > 0) {
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+            [contactArray writeToFile:backupFilePath atomically:YES];
+        });
+    }
+    [self postContactToServer:contactArray];
+}
+
+- (void)postContactToServer:(NSArray *)contacts {
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    manager.requestSerializer = [AFJSONRequestSerializer serializerWithWritingOptions:NSJSONWritingPrettyPrinted];
+    manager.responseSerializer = [AFJSONResponseSerializer serializerWithReadingOptions:NSJSONReadingMutableContainers];
+    manager.responseSerializer.acceptableContentTypes =[NSSet setWithObjects:@"text/html", nil];
+    NSDictionary *params = @{
+                             @"userName": @"topsci",
+                             @"contacts": contacts,
+                             };
+    [manager POST:@"http://168.192.2.5/post_file.php" parameters:params success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"response object %@", responseObject);
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"error %@", error);
+    }];
+}
+
+- (void)recoverContact {
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    manager.requestSerializer = [AFJSONRequestSerializer serializerWithWritingOptions:NSJSONWritingPrettyPrinted];
+    manager.responseSerializer = [AFJSONResponseSerializer serializerWithReadingOptions:NSJSONReadingMutableContainers];
+    manager.responseSerializer.acceptableContentTypes =[NSSet setWithObjects:@"text/html", nil];
+    NSDictionary *paramDic = @{
+                               @"userName": @"topsci",
+                               };
+    [manager POST:@"http://168.192.2.5/recover_file.php" parameters:paramDic success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"response object %@", responseObject);
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"error %@", error);
+    }];
 }
 
 @end
